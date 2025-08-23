452ee8c1a6d3ac69452c0b6a159ba6d594330016
polcm005 <polcm005@mymail.unisa.edu.au>
2025-08-23 17:37:33 +0930

    Week 3 - Practical - Add non-mapped model properties, FullName and ContactNumber, to replace first and last names, and main and secondary phone number from search results

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AOWebApp2.Models;
 
@@ -7,14 +10,18 @@ public partial class Customer
 {
     public int CustomerId { get; set; }
 
+    [Display(Name = "First Name")]
     public string FirstName { get; set; } = null!;
 
+    [Display(Name = "Last Name")]
     public string LastName { get; set; } = null!;
 
     public string Email { get; set; } = null!;
 
+    [Display(Name = "Main Phone Number")]
     public string MainPhoneNumber { get; set; } = null!;
 
+    [Display(Name = "Secondary Phone Number")]
     public string? SecondaryPhoneNumber { get; set; }
 
     public int AddressId { get; set; }
@@ -24,4 +31,32 @@ public partial class Customer
     public virtual ICollection<CustomerOrder> CustomerOrders { get; set; } = new List<CustomerOrder>();
 
     public virtual ICollection<Review> Reviews { get; set; } = new List<Review>();
+
+    [NotMapped][Display(Name = "Customer Name")] public string FullName => FirstName + " " + LastName;
+
+    [NotMapped]
+    [Display(Name = "Contact Number")]
+    public string ContactNumber
+    {
+        get
+        {
+            if (!MainPhoneNumber.IsNullOrEmpty() && SecondaryPhoneNumber.IsNullOrEmpty())
+            {
+                return MainPhoneNumber;
+            }
+            else if (!MainPhoneNumber.IsNullOrEmpty() && !SecondaryPhoneNumber.IsNullOrEmpty())
+            {
+                return MainPhoneNumber + " : " + SecondaryPhoneNumber;
+            }
+            else if (MainPhoneNumber.IsNullOrEmpty() && !SecondaryPhoneNumber.IsNullOrEmpty())
+            {
+                return SecondaryPhoneNumber;
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+
 }
