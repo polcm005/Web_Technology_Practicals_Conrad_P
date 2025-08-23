2721c2d3dd732e026086ae3b263068bfee1801bb
polcm005 <polcm005@mymail.unisa.edu.au>
2025-08-23 23:59:16 +0930

    Week 4 - Practical - Add ItemIndex.html static HTML file. Also create ItemsReview.cs (Week 3 Activity) and CustomerSearchVM.cs (Week 3 Practical)

---
+++ ViewModels/CustomerSearchVM.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AOWebApp2.ViewModels
+{
+    public class CustomerSearchVM
+    {
+        public string SearchText { get; set; }
+        public string Suburb { get; set; }
+        public SelectList SuburbList { get; set; }
+        public List<Models.Customer> CustomerList { get; set; }
+    }
+}
---
+++ ViewModels/ItemsReview.cs
@@ -0,0 +1,11 @@
+﻿using AOWebApp2.Models;
+
+namespace AOWebApp2.ViewModels
+{
+    public class ItemsReview
+    {
+        public Item ItemSubject { get; set; }
+        public double averageRating { get; set; }
+        public int reviewQuantity { get; set; }
+    }
+}
